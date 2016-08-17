c1f60573fa0ab3c540a11e4ccf6f7db00db2ce48
wanaaaa <wanaaaa@gmail.com>
2016-08-17 11:44:17 -0400

    nytimes

--- Yelp/NYTimesWan/MyApplication/app/src/main/java/com/example/ubun17/nytimes/MainActivity.java
+++ Yelp/NYTimesWan/MyApplication/app/src/main/java/com/example/ubun17/nytimes/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.ubun17.nytimes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -26,10 +29,15 @@ import okhttp3.ResponseBody;
 
 public class MainActivity extends AppCompatActivity {
     String nyAPIkey = "48c66fa2c448eda40826487d4f19a018:0:71658152";
-    String nyAPIurl = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=general assembly&sort=newest&&api-key=48c66fa2c448eda40826487d4f19a018:0:71658152";
+    String nyAPIurl = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=shake shack&sort=newest&&api-key=48c66fa2c448eda40826487d4f19a018:0:71658152";
+    String baseURL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=";
+
     ArrayList<String> nyArray = new ArrayList<String>();
     ArrayAdapter adapter;
     ListView listview;
+
+    EditText searchTerm;
+    Button buSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +47,26 @@ public class MainActivity extends AppCompatActivity {
         listview = (ListView) findViewById(R.id.listView);
         adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, nyArray);
         listview.setAdapter(adapter);
+        buSearch = (Button) findViewById(R.id.button);
+        searchTerm = (EditText) findViewById(R.id.searchEdit);
 
+        buSearch.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                String searchString = searchTerm.getText().toString();
+                String finalAPIstring = baseURL+searchString+"&sort=newest&&api-key="+ nyAPIkey;
+                Log.d("url : ", finalAPIstring);
+                returnAPIcall(finalAPIstring);
+            }
+        });
+
+    }//End of onCreate
+
+    public void returnAPIcall(String finalURL){
         OkHttpClient client = new OkHttpClient();
 
         final Request apiRequest = new Request.Builder()
-                .url(nyAPIurl)
+                .url(finalURL)
                 .build();
 
         client.newCall(apiRequest).enqueue(new Callback() {
@@ -58,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
                     Log.i("Connection:","No response");
                 } else {
                     String stBody = response.body().string();
-
+                    nyArray.clear();
                     Gson gson = new Gson();
                     NYtimesResult nyTimesResult = gson.fromJson(stBody, NYtimesResult.class);
 
@@ -69,10 +92,16 @@ public class MainActivity extends AppCompatActivity {
                                 .get(i).getLeadParagraph();
                         nyArray.add(snippet);
                     }
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            adapter.notifyDataSetChanged();
+                        }
+                    });
                 }
             }
         }); //End of newCall
-    }
 
-
+    }//end of returnAPIcall
 }//End of MainActivity
