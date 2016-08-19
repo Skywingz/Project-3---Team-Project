0645d3c64432f8a14899946d58836a7c24e92cce
wanaaaa <wanaaaa@gmail.com>
2016-08-19 15:37:29 -0400

    asdf

--- YelpQuest/app/src/androidTest/java/immersive/android/assembly/general/yelpquest/MainActivityTest.java
+++ YelpQuest/app/src/androidTest/java/immersive/android/assembly/general/yelpquest/MainActivityTest.java
@@ -15,7 +15,6 @@ import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
-
 /**
  * Created by ubun17 on 8/19/16.
  */
